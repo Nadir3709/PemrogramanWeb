d9a50d907249401f62354b754e05d7ae068c8acf
Nadir <muhamadnadir6709@gmail.com>
2025-08-23 07:29:18 +0700

    Pindahkan semua file proyek 1 ke dalam folder

--- script.js
+++
@@ -1,7 +0,0 @@
-const menuBtn = document.getElementById('menu-btn');
-const navMenu = document.getElementById('nav-menu');
-
-menuBtn.addEventListener('click', () => {
-    menuBtn.classList.toggle('active');
-    navMenu.classList.toggle('active');
-});
